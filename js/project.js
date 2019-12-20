d9601f18cffd8bbd362c62799b50a948c711606d
kimhuy.soth <kimhuy.soth@pnc.lan>
2019-12-20 20:05:40 +0700

    add new

--- js/project.js
+++ js/project.js
@@ -1,107 +1,183 @@
-function getUrl() {
+// getURL function for control url
+function getURL() {
     var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
     return url;
 }
 
+// jQuery
 $(document).ready(function () {
-    requestApi();
-    //catch value
+    // call function for request API
+    requestdata();
+    // Select recipes with id
+    $("#chooseRecipe").on('change', function () {
+        var chooseRecipse = $("#chooseRecipe").val();
+        recipesId(chooseRecipse);
+    });
 
-    $('#recipe').on('change', () => {
-        var recipes = $('#recipe').val();
-        // console.log(recipes);
-        //call function
-        getRecipe(recipes);
-    })
+    $('#minus').on('click', function () {
+        decrease();
+        var guest = $('#member').val();//old member 4 it will decrease when click -
+        var recipe = $('#chooseRecipe').val();//id from select
+        newIngredient(recipe, guest);
+    });
+    $('#add').on('click', function () {
+        increase();
+        var guest = $('#member').val();//old member 4 it will increase when click +
+        var recipe = $('#chooseRecipe').val();
+       
+        newIngredient(recipe, guest);
+    });
 });
 
-function requestApi() {
+// function for query API
+function requestdata() {
     $.ajax({
         dataType: 'json',
-        url: getUrl(),
-        success: (data) => chooseRecipe(data.recipes),
-        error: () => console.log("Cannot get data"),
-    });
+        url: getURL(),
+        success: (data) => getRecipse(data.recipes),
+        error: () => console.log("Cannot get data")
+    })
 }
 
-
+//variable for store data in arrays that get from API
 var allData = [];
-function chooseRecipe(recipe) {
-    allData = recipe;
+// function for display the name of food on the selection option
+function getRecipse(data) {
     var option = "";
-    recipe.forEach(item => {
-        //console.log(item)
-        option += `<option value="${item.id}">${item.name}</option>`;
+    allData = data;
+    data.forEach(element => {
+        option += `<option value="${element.id}">${element.name}</option>`;
     });
-    $('#recipe').append(option);
+    $("#chooseRecipe").append(option);
+    $('#ruler').hide();
+    $('#show-number').hide();
 }
-function getRecipe(id) {
-    //     console.log(recipeId);
+// variable for get old Guest
+var getQuanlity = [];
+// function for loop data from array variable
 
-    allData.forEach(item => {
-        //console.log(item);
 
-        if (item.id == id) {
-            // console.log(item);
-            //showRecipe(),
-            showRecipe(item.name, item.iconUrl);
-            //showIngredient(),
-            showIngredient(item.ingredients);
-            //showStep()...
-            getInstruction(item);
-        }
-    });
-}
-
-function showRecipe(name, img) {
+// display name and image of recipes
+function chooseRecipses(item) {
+    const { name, iconUrl } = item;
     var result = "";
     result += `
-        <tr>
-            <td><h2>${name}</h2></td>
-            <td> <img src="${img}" class="img-fluid" width="500px"> </td>
-        </tr>
-     `;
-
-    $('#recipe-data').html(result);
+       <tr>
+          <td> <h4>${name}</h4></td>
+          <td><img src="${iconUrl}" style="width:250px; height:170px"></td> 
+       </tr> 
+    `;
+    $("#result").html(result);
 }
 
-function showIngredient(ing){
-     var ingredients = " ";
-    ing.forEach(element => {
-        //console.log(item);
-         ingredients += `
-        <tr>
-            <td> <img src="${element.iconUrl}" width="100" class="img-fluid"></td>
-            <td>${element.quantity}</td>
-            <td>${element.unit[0]}</td>
-            <td> ${element.name}</td>
-        </tr>
-      `;
-    }); 
-     $('#ingredien').html(ingredients);
-}
-
-
-      //Step
-      function getInstruction(element) {
-       const{instructions} = element;
-        var instruction = "";
-        var step = instructions.split("<step>");
-        for(let k =1; k<step.length; k++){
-            instruction += `
-                <h4 class="text-info">step ${k}:</h4>
-                ${step[k]}
-            `;
+function recipesId(chooseRecipse) {
+    allData.forEach(item => {
+        if (item.id == chooseRecipse) {
+            // choose recipses 
+            chooseRecipses(item);
+            // get ingredients in recipse name
+            getIngrediants(item);
+            // get instruction
+            getInstruction(item);
+            //updateIngredien
+          $('#member').val(item.nbGuests);
+          oldGuest = $('#member').val();
+            //get OldGuest
         }
+        
+    });
+}
+function getIngrediants(item) {
+    var result = "";
+    item.ingredients.forEach(element => {
+        const { name, quantity, unit, iconUrl } = element;
+        result += `
+            <tr>
+            <td><img src="${iconUrl}" style="width:50px"></td>
+            <td id='quantity'>${quantity}</td>
+            <td>${unit[0]}</td>
+            <td>${name}</td>
+            </tr>
+        `;
+    });
+    $("#ingredients").html(result);
+    $('#ruler').show();
+    $('#show-number').show();
 
-        $('#step').html(instruction);
-    }      
+}
+function newIngredient(chooseRecipse,guest) {
+
+    allData.forEach(item => {
+        if (item.id == chooseRecipse) {
+            // choose recipses 
+            chooseRecipses(item);
+            // get ingredients in recipse name
+            updateIngredient(item.ingredients,guest);
+              
+            // get instruction
+            getInstruction(item);
+            //updateIngredien
+            $('#member').val(guest);
+            //get OldGuest
+        }
+    });
+}
+// get ingredients in recipse name
+
+// function for new quanlity
+
+var updateIngredient = (ing,guest) => {
+ 
+    var ingredient = "";
+    ing.forEach(element => {
+  
+       var add = element.quantity *parseInt(guest) /oldGuest;
+       ingredient += `
+       <tr>
+           <td><img src = "${element.iconUrl}" width = "50"></td>
+      
+           <td >${add}</td>
+           <td >${element.unit[0]}</td>
+           <td >${element.name}</td>
+         
+       </tr>
+     `
+    })
+    $('#ingredients').html(ingredient);
+} 
 
 
+// get instruction
+function getInstruction(item) {
+    const { instructions } = item;
+    var instruction = "";
+    var step = instructions.split("<step>");
+    for (let k = 1; k < step.length; k++) {
+        instruction += `
+      <h5 class="text-primary">step ${k}:</h5>
+             ${step[k]}
+        `;
+    }
+    // diplay instruction
+    $('#step').html(instruction);
+}
 
+//increase and decrement
+function increase() {
+    var member = $('#member').val();
+    var guest = parseInt(member) + 1;
+    if (guest <= 15) {
+        $('#member').val(guest);
+    }
+}
 
-
-
+function decrease() {
+    var member = $('#member').val();
+    var guest = parseInt(member) - 1;
+    if (guest >= 1) {
+        $('#member').val(guest);
+    }
+}
 
 
 
